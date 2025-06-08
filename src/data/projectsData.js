df97b1d5ff2568d2ec831cabdf7ae52ee789d6ba
chiar <chiar.evdi@gmail.com>
2025-06-08 22:01:42 +0200

    deploy the projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,7 +11,7 @@ export const projectsData = [
       'A fully interactive task management application built with React. Features include drag-and-drop functionality to organize tasks, dynamic creation and deletion of tasks, and a celebratory confetti effect upon completion.',
     image: kanbanImage,
     tags: ['React', 'Styled-Components', '@hello-pangea/dnd', 'JavaScript'],
-    liveUrl: 'https://kanban-app-chiar.vercel.app', 
+    liveUrl: 'https://kanban-task-app-blond.vercel.app/', 
     repoUrl: 'https://github.com/chiarabdy/kanban-task-app',
   },
   // {
